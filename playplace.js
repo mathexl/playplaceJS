ec699d5ecc00a64635665845e56f79f2c6a8451a
Mathew Pregasen <mathexl@gmail.com>
2017-01-01 14:26:12 -0500

    adding marraige scaffolding

--- playplace.js
+++ playplace.js
@@ -49,60 +49,99 @@ $(document).ready(function(){
     window.playplace.interval(fnc);
   }
 
+  $(".playplace .marriage.width_height").click(function(){
+    if(window.playplace.width_height == true){
+      window.playplace.width_height = false;
+      $(".playplace .marriage.width_height").removeClass("active");
+    } else {
+      window.playplace.width_height = true;
+      $(".playplace .marriage.width_height").addClass("active");
+    }
+  })
+
+  window.playplace.height_inc = function(){
+     height = window.playplace.height;
+     if($(window.playplace.el).height(height + 1)){
+       $(".playplace_height input").val(height + 1);
+       window.playplace.height++;
+     }
+   }
+
+  window.playplace.height_desc = function(){
+     height = window.playplace.height;
+     if($(window.playplace.el).height(height - 1)){
+       $(".playplace_height input").val(height - 1);
+       window.playplace.height--;
+     }
+   }
+
+   window.playplace.width_inc = function(){
+      width = window.playplace.width;
+      if($(window.playplace.el).width(width + 1)){
+        $(".playplace_width input").val(width + 1);
+        window.playplace.width++;
+
+      }
+    }
+
+  window.playplace.width_desc = function(){
+     width = window.playplace.width;
+     if($(window.playplace.el).width(width - 1)){
+       $(".playplace_width input").val(width - 1);
+       window.playplace.width--;
+     }
+   }
+
   $(".playplace_height .uparrow").mousedown(function(){
     window.playplace.acceleration = 200;
     window.playplace.engine(
-      function(){
-         height = window.playplace.height;
-         if($(window.playplace.el).height(height + 1)){
-           $(".playplace_height input").val(height + 1);
-           window.playplace.height++;
-
-         }
-       }
+      function () {
+        window.playplace.height_inc();
+        if(window.playplace.width_height == true){
+          window.playplace.width_inc();
+        }
+      }
     );
   });
+
+
 
   $(".playplace_height .downarrow").mousedown(function(){
     window.playplace.acceleration = 200;
     window.playplace.engine(
-      function(){
-         height = window.playplace.height;
-         if($(window.playplace.el).height(height - 1)){
-           $(".playplace_height input").val(height - 1);
-           window.playplace.height--;
-
-         }
-       }
+      function () {
+        window.playplace.height_desc();
+        if(window.playplace.width_height == true){
+          window.playplace.width_desc();
+        }
+      }
     );
   });
+
+
 
 
   $(".playplace_width .uparrow").mousedown(function(){
     window.playplace.acceleration = 200;
     window.playplace.engine(
-      function(){
-         width = window.playplace.width;
-         if($(window.playplace.el).width(width + 1)){
-           $(".playplace_width input").val(width + 1);
-           window.playplace.width++;
-
-         }
-       }
+      function () {
+        window.playplace.width_inc();
+        if(window.playplace.width_height == true){
+          window.playplace.height_inc();
+        }
+      }
     );
   });
 
   $(".playplace_width .downarrow").mousedown(function(){
     window.playplace.acceleration = 200;
     window.playplace.engine(
-      function(){
-         width = window.playplace.width;
-         if($(window.playplace.el).width(width - 1)){
-           $(".playplace_width input").val(width - 1);
-           window.playplace.width--;
-
-         }
-       }
+      function () {
+        window.playplace.width_desc();
+        if(window.playplace.width_height == true){
+          window.playplace.height_desc();
+        }
+      }
     );
   });
 
